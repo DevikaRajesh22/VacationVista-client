e1ec783ef8aeed1173e7d2b9748716c91c4d1b2f
DevikaRajesh22 <devikaraj699@gmail.com>
2024-04-09 17:27:14 +0530

    Buyer homepage made dynamic

--- src/Components/Buyer/Trend.tsx
+++ src/Components/Buyer/Trend.tsx
@@ -1,117 +1,71 @@
-import trend1 from "../../assets/trend1.png";
-import trend2 from "../../assets/trend2.png";
-import trend3 from "../../assets/trend3.png";
-import trend4 from "../../assets/trend4.png";
+import { useState, useEffect } from 'react';
+import {property} from '../../Api/admin'
 
-interface CardProps {
-  image: string;
-  title: string;
-  price: string;
+interface Property {
+  id: string,
+  title: string,
+  address: string,
+  photos: string,
+  price: string,
+  status: string,
+  isBlocked: boolean,
 }
 
-const Cards: React.FC<CardProps> = ({ image, title, price }) => {
-  return (
-    <div className="w-full max-w-xs bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-      <a href="#">
-        <img
-          className="p-3 rounded-t-lg max-w-full h-auto"
-          src={image}
-          alt="product image"
-        />
-      </a>
-      <div className="px-3 pb-3">
-        <a href="#">
-          <h5 className="text-lg font-semibold tracking-tight text-gray-900 dark:text-white">
-            {title}
-          </h5>
-        </a>
-        <div className="flex items-center mt-1 mb-2">
-          <div className="flex items-center space-x-1 rtl:space-x-reverse">
-            <svg
-              className="w-2.5 h-2.5 text-yellow-300"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="currentColor"
-              viewBox="0 0 22 20"
-            >
-              <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-            </svg>
-            <svg
-              className="w-2.5 h-2.5 text-yellow-300"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="currentColor"
-              viewBox="0 0 22 20"
-            >
-              <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-            </svg>
-            <svg
-              className="w-2.5 h-2.5 text-yellow-300"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="currentColor"
-              viewBox="0 0 22 20"
-            >
-              <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-            </svg>
-            <svg
-              className="w-2.5 h-2.5 text-yellow-300"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="currentColor"
-              viewBox="0 0 22 20"
-            >
-              <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-            </svg>
-            <svg
-              className="w-2.5 h-2.5 text-gray-200 dark:text-gray-600"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="currentColor"
-              viewBox="0 0 22 20"
-            >
-              <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-            </svg>
-          </div>
-          <span className="bg-blue-100 text-blue-800 text-xs font-semibold px-2 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ms-2">
-            5.0
-          </span>
-        </div>
-        <div className="flex items-center justify-between">
-          <span className="text-base font-bold text-gray-900 dark:text-white">
-            ₹{price}
-          </span>
-        </div>
-      </div>
-    </div>
-  );
-};
-
 const Trend = () => {
+  const [properties, setProperties] = useState<Property[]>([]);
+
+  useEffect(()=>{
+    const fetchPropertyData=async()=>{
+      try{
+        const res = await property()
+        if (res?.data.success) {
+          setProperties(res.data.getProperty)
+        }
+      }catch(error){
+        console.log(error)
+      }
+    }
+    fetchPropertyData()
+  },[]);
+  console.log(properties)
+
   return (
-    <div>
-      <div className="text-center pb-8 pt-10">
-        <h3 className="font-semibold text-lg md:text-xl lg:text-2xl">
-          Trending
-        </h3>
+    <section className="bg-white py-12 text-gray-700 sm:py-16 lg:py-20">
+      <div className="mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-8">
+        <div className="mx-auto max-w-md text-center">
+          <h3 className='font-bold'>Trending</h3>
+        </div>
+        <div className="mt-10 grid grid-cols-2 gap-6 lg:mt-16 lg:grid-cols-4 lg:gap-4">
+          {properties.slice(0, 8).map((val) => {
+            return (
+              <div className={`${(val.status == 'Verification Required' || val.status == "Rejected" || val.isBlocked == true) && 'hidden'}`}>
+                <article className="relative">
+                  <div className="aspect-square overflow-hidden">
+                    <img
+                      className="group-hover:scale-125 h-full w-full object-cover transition-all duration-300"
+                      src={val.photos[0]}
+                      alt=""
+                    />
+                  </div>
+                  <div className="mt-4 flex items-start justify-between">
+                    <div className="">
+                      <h3 className="text-xs font-semibold sm:text-sm md:text-base text-black">
+                        <a href="#" title="" className="cursor-pointer">
+                          {val.title}
+                        </a>
+                      </h3>
+                      <p className='text-black'>
+                        <span className='font-semibold'>₹{val.price}</span> night
+                      </p>
+                    </div>
+                  </div>
+                </article>
+              </div>
+            )
+          })}
+        </div>
       </div>
-      <div className="container px-4 py-8 mx-auto lg:px-8">  
-      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-        <Cards image={trend1} title="Jambiani, Tanzania" price="19,843" />
-        <Cards image={trend2} title="Kecamatan, Indonesia" price="26,335" />
-        <Cards image={trend3} title="Vagator, India" price="1,11,400" />
-        <Cards image={trend4} title="Goa, India" price="2,03,400" />
-        <Cards image={trend1} title="Jambiani, Tanzania" price="19,843" />
-        <Cards image={trend2} title="Kecamatan, Indonesia" price="26,335" />
-        <Cards image={trend3} title="Vagator, India" price="1,11,400" />
-        <Cards image={trend4} title="Goa, India" price="2,03,400" />
-        <Cards image={trend1} title="Jambiani, Tanzania" price="19,843" />
-        <Cards image={trend2} title="Kecamatan, Indonesia" price="26,335" />
-        <Cards image={trend3} title="Vagator, India" price="1,11,400" />
-        <Cards image={trend4} title="Goa, India" price="2,03,400" />
-      </div>
-      </div>
-    </div>
+    </section>
   );
 };
 
