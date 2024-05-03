2e3427d95e3242a132b5e102f80b3fdce4ad2347
DevikaRajesh22 <devikaraj699@gmail.com>
2024-05-03 11:16:31 +0530

    added more info in buyer profile

--- src/Components/Buyer/EditBuyerProfile.tsx
+++ src/Components/Buyer/EditBuyerProfile.tsx
@@ -6,76 +6,77 @@ import { editProfile } from "../../Api/buyer";
 
 
 const EditBuyerProfile = () => {
-  const [name,setName]=useState('')
-  const [date,setDate]=useState('')
-  const [phone,setPhone]=useState('')
-  const [govt,setGovt]=useState('')
-  const [image,setImage]=useState<File | null>(null);
-  const navigate=useNavigate()
+  const [name, setName] = useState('')
+  const [date, setDate] = useState('')
+  const [phone, setPhone] = useState('')
+  const [govtId, setGovtId] = useState('')
+  const [image, setImage] = useState<File | null>(null);
+  const navigate = useNavigate()
 
-  const handleSubmit=async(e: React.FormEvent<HTMLFormElement>)=>{
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
-    try{
-      if(name.trim().length<2){
+    try {
+      if (name.trim().length < 2) {
         toast.error('Please enter valid name !!')
         return;
-      }else if(phone.trim().length!=10){
+      } else if (phone.trim().length != 10) {
         toast.error('Phone number should contain 10 characters !!')
         return;
-      }else if(govt.trim().length!=12){
+      } else if (govtId.trim().length != 12) {
         toast.error('Wrong government ID')
         return;
       }
       const formData = new FormData();
-      formData.append('name',name)
-      formData.append('dateOfBirth',date)
-      formData.append('phone',phone)
-      formData.append('govt',govt)
-      if(image){
-        formData.append('image',image)
+      formData.append('name', name)
+      formData.append('dateOfBirth', date)
+      formData.append('phone', phone)
+      formData.append('govtId', govtId)
+      if (image) {
+        formData.append('image', image)
       }
-      const res=await editProfile(formData)
-      if(res?.data.success){
+      const res = await editProfile(formData)
+      if (res?.data.success) {
+        toast.success('Profile updated..')
         navigate('/profile')
-      }else{
+      } else {
         toast.error('Something went wrong...')
       }
 
-    }catch(error){
+    } catch (error) {
       console.log(error)
     }
   }
 
-  const handleImage=(e: React.ChangeEvent<HTMLInputElement>)=>{
+  const handleImage = (e: React.ChangeEvent<HTMLInputElement>) => {
     const selectedFile = e.target.files?.[0];
-    if(selectedFile){
+    if (selectedFile) {
       setImage(selectedFile)
     }
   }
-  
+
 
   return (
     <div className="flex min-h-screen items-center justify-center">
       <div className="bg-white shadow-md rounded-lg px-8 pb-8 max-w-md mb-20">
         <form className="max-w-md mx-auto" onSubmit={handleSubmit}>
           <h3 className="mb-5 mt-5 font-semibold">Edit your profile</h3>
-            <div className="relative z-0 w-full mb-5 group">
-              <input
-                type="text"
-                value={name}
-                onChange={(e)=>setName(e.target.value)}
-                name="floating_first_name"
-                id="floating_first_name"
-                className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-                placeholder=" "
-              />
-              <label
-                htmlFor="floating_first_name"
-                className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
-              >
-                Legal Name
-              </label>
-            </div>
+          <div className="relative z-0 w-full mb-5 group">
+            <input
+              type="text"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              name="floating_first_name"
+              id="floating_first_name"
+              className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+              placeholder=" "
+            />
+            <label
+              htmlFor="floating_first_name"
+              className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+            >
+              Legal Name
+            </label>
+          </div>
           <div className="relative z-0 w-full mb-5 group">
             <label
               htmlFor="floating_repeat_password"
@@ -86,7 +87,7 @@ const EditBuyerProfile = () => {
             <input
               type="date"
               value={date}
-              onChange={(e)=>setDate(e.target.value)}
+              onChange={(e) => setDate(e.target.value)}
               name="repeat_password"
               id="floating_repeat_password"
               className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
@@ -98,7 +99,7 @@ const EditBuyerProfile = () => {
               <input
                 type="tel"
                 value={phone}
-                onChange={(e)=>setPhone(e.target.value)}
+                onChange={(e) => setPhone(e.target.value)}
                 name="floating_phone"
                 id="floating_phone"
                 className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
@@ -114,8 +115,8 @@ const EditBuyerProfile = () => {
             <div className="relative z-0 w-full mb-5 group">
               <input
                 type="text"
-                value={govt}
-                onChange={(e)=>setGovt(e.target.value)}
+                value={govtId}
+                onChange={(e) => setGovtId(e.target.value)}
                 name="floating_company"
                 id="floating_company"
                 className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
--- src/Components/Buyer/Profile.tsx
+++ src/Components/Buyer/Profile.tsx
@@ -1,34 +1,55 @@
 import user from '../../assets/user.png'
 import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import {profile} from '../../Api/buyer'
+import { profile } from '../../Api/buyer'
+
+interface Buyer {
+  id: string,
+  name: string,
+  email: string,
+  image: string,
+  password: string,
+  isBlocked: boolean,
+  dateOfBirth: Date,
+  phone: string,
+  govtId: string,
+  creationTime: Date,
+}
 
 const Profile = () => {
   const [image, setImage] = useState<string | null>(null);
-  const [name,setName]=useState('');
-  const [email,setEmail]=useState('');
+  const [buyer, setBuyer] = useState<Buyer>();
 
-  useEffect(()=>{
-    const fetchUserData=async()=>{
-      try{
-        const res=await profile()
-        console.log('res',res?.data.buyerProfile)
-        if(res?.data?.buyerProfile){
-          setEmail(res.data.buyerProfile.email);
-          setName(res.data.buyerProfile.name);
+  useEffect(() => {
+    const fetchUserData = async () => {
+      try {
+        const res = await profile()
+        if (res?.data?.buyerProfile) {
+          setBuyer(res.data.buyerProfile)
           setImage(res.data.buyerProfile.image || user);
         }
-      }catch(error){
+      } catch (error) {
         console.log(error)
       }
     }
     fetchUserData()
-  },[]);
+  }, []);
+
+  function formatDate(dateString: Date) {
+    const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    const date = new Date(dateString);
+    const day = date.getDate();
+    const month = months[date.getMonth()];
+    const year = date.getFullYear();
+
+    return `${day} ${month}, ${year}`;
+  }
 
   const src = image || user;
+
   return (
     <div className="flex justify-center items-start h-screen">
-        <div className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 flex flex-col justify-center items-center mt-20">
+      <div className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 flex flex-col justify-center items-center mt-20">
         <div className="flex justify-end px-4 pt-4">
         </div>
         <div className="flex flex-col items-center pb-10">
@@ -38,11 +59,31 @@ const Profile = () => {
             alt="Bonnie image"
           />
           <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">
-            {name}
+            {buyer?.name}
           </h5>
           <span className="text-sm text-gray-500 dark:text-gray-400">
-            {email}
+            Email : {buyer?.email}
           </span>
+          {
+            buyer?.phone &&
+            <span className="text-sm text-gray-500 dark:text-gray-400">
+              Contact : {buyer?.phone}
+            </span>
+          }
+          {buyer?.dateOfBirth && <span className="text-sm text-gray-500 dark:text-gray-400">
+            DOB : {formatDate(buyer?.dateOfBirth)}
+          </span>}
+          {
+            buyer?.govtId &&
+            <span className="text-sm text-gray-500 dark:text-gray-400">
+              Government ID : {buyer?.govtId}
+            </span>
+          }
+          {buyer?.creationTime && <span className="text-sm text-gray-500 dark:text-gray-400">
+            Since {formatDate(buyer?.creationTime)}
+          </span>}
+
+
           <div className="flex mt-4 md:mt-6">
             <Link
               to='/editProfile'
