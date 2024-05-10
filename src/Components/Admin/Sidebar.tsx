eec5d7bd8492598945328ab604cbb17dc05c310a
DevikaRajesh22 <devikaraj699@gmail.com>
2024-05-10 15:37:58 +0530

    Admin side made responsive

--- src/Components/Admin/Sidebar.tsx
+++ src/Components/Admin/Sidebar.tsx
@@ -7,11 +7,13 @@ import { admLogout } from '../../Store/slice/authSlice';
 import { useDispatch } from 'react-redux';
 import { toast } from 'react-toastify'
 import { useNavigate } from 'react-router-dom';
+import { useState, useRef, useEffect } from 'react'
 
 const SidebarWithLogo = () => {
-
+  const [openSidebar, setOpenSidebar] = useState(false)
   const dispatch = useDispatch()
   const navigate = useNavigate()
+  const sidebarRef = useRef(null);
 
   const handleLogout = async () => {
     try {
@@ -25,6 +27,28 @@ const SidebarWithLogo = () => {
       console.log(error)
     }
   }
+
+  const handleCloseSidebar = () => {
+    setOpenSidebar(false);
+  };
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (sidebarRef.current && !sidebarRef.current.contains(event.target)) {
+        setOpenSidebar(false);
+      }
+    };
+
+    if (openSidebar) {
+      document.addEventListener('mousedown', handleClickOutside);
+    } else {
+      document.removeEventListener('mousedown', handleClickOutside);
+    }
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [openSidebar]);
   return (
     <>
       <button
@@ -32,6 +56,7 @@ const SidebarWithLogo = () => {
         data-drawer-toggle="logo-sidebar"
         aria-controls="logo-sidebar"
         type="button"
+        onClick={() => setOpenSidebar(!openSidebar)}
         className="inline-flex items-center p-2 mt-2 ms-3 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
       >
         <span className="sr-only">Open sidebar</span>
@@ -51,10 +76,32 @@ const SidebarWithLogo = () => {
       </button>
       <aside
         id="logo-sidebar"
-        className="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0"
+        ref={sidebarRef}
+        className={`fixed top-0 left-0 z-40 w-64 h-screen transition-transform ${
+          openSidebar ? '' : '-translate-x-full sm:translate-x-0'
+        }`}
         aria-label="Sidebar"
       >
         <div className="h-full px-3 py-4 overflow-y-auto bg-blue-900 dark:bg-gray-800">
+        <button
+            onClick={handleCloseSidebar}
+            className="absolute top-2 right-2 p-1 text-gray-500 rounded-full hover:bg-gray-200 dark:hover:bg-gray-700 focus:outline-none focus:bg-gray-200 dark:focus:bg-gray-700"
+          >
+            <svg
+              className="w-6 h-6"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M6 18L18 6M6 6l12 12"
+              />
+            </svg>
+          </button>
           <a href="https://flowbite.com/" className="flex items-center ps-2.5 mb-5">
             <img
               src={logo}
