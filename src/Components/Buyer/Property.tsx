83330892f7fa11e069a80ef7409ae3390238079a
DevikaRajesh22 <devikaraj699@gmail.com>
2024-05-19 09:16:24 +0530

    ux improved

--- src/Components/Buyer/Property.tsx
+++ src/Components/Buyer/Property.tsx
@@ -142,8 +142,8 @@ const Property = () => {
                       }
                     }} className="h-full rounded-l border block appearance-none w-full bg-white border-gray-400 text-gray-700 py-2 px-4 pr-8 leading-tight focus:outline-none focus:bg-white focus:border-gray-500">
                       <option>Sort</option>
-                      <option>Low to High</option>
-                      <option>High to Low</option>
+                      <option>Price: Low to High</option>
+                      <option>Price: High to Low</option>
                     </select>
                     <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
                       <svg
