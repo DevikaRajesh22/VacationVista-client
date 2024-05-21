a3fda26da00980be13d81ed305994bb746a87492
DevikaRajesh22 <devikaraj699@gmail.com>
2024-05-21 12:43:26 +0530

    sort error

--- src/Components/Buyer/Property.tsx
+++ src/Components/Buyer/Property.tsx
@@ -135,15 +135,15 @@ const Property = () => {
                   <div className="relative">
                     <select onChange={(e) => {
                       const selectedOption = e.target.value;
-                      if (selectedOption == 'Sort') {
+                      if (selectedOption == 'Sort By Price') {
                         setSortOption('')
                       } else {
                         setSortOption(selectedOption)
                       }
                     }} className="h-full rounded-l border block appearance-none w-full bg-white border-gray-400 text-gray-700 py-2 px-4 pr-8 leading-tight focus:outline-none focus:bg-white focus:border-gray-500">
                       <option>Sort</option>
-                      Price: <option>Low to High</option>
-                      Price: <option>High to Low</option>
+                      <option>Low to High</option>
+                      <option>High to Low</option>
                     </select>
                     <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
                       <svg
